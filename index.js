a050aa8582d62d813f7cc3f80f564c0b278da6d5
Azumabashi <hiro_kataoka@hkataoka.local>
2022-04-11 15:52:11 +0900

    add ;

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const server = http
             rawData += chunk;
           })
           .on('end', () => {
-            const answer = new URLSearchParams(rawData)
+            const answer = new URLSearchParams(rawData);
             const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
             console.info(body);
             res.write(`<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`);
